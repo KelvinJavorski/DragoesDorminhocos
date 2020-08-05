e782a8ffc25744ea49016cb9a55a438ead5de379
Kelvin <kelvin.javorskisoares@gmail.com>
2020-08-05 16:03:22 -0300

    Added BattleState Enum and BattleManager Class

---
+++ Dragon Dreamers/Dragon Dreamers/Model/BattleManager.swift
@@ -0,0 +1,17 @@
+//
+//  BattleManager.swift
+//  Dragon Dreamers
+//
+//  Created by Kelvin Javorski Soares on 05/08/20.
+//  Copyright © 2020 Kelvin Javorski Soares. All rights reserved.
+//
+
+import Foundation
+
+class BattleManager{
+    var player: Player!
+    //var enemy: Enemy
+    var battleState: BattleState = .playerTurn
+    
+    
+}
---
+++ Dragon Dreamers/Dragon Dreamers/Model/Enums/BattleState.swift
@@ -0,0 +1,20 @@
+//
+//  BattleState.swift
+//  Dragon Dreamers
+//
+//  Created by Kelvin Javorski Soares on 05/08/20.
+//  Copyright © 2020 Kelvin Javorski Soares. All rights reserved.
+//
+
+import Foundation
+
+enum BattleState{
+    case playerTurn
+    case playerCardEffect
+    case enemyTurn
+    case enemyCardEffect
+    case PlayerWonLife
+    case playerWonOpinion
+    case enemyWonLife
+    case enemyWonOpinion
+}
