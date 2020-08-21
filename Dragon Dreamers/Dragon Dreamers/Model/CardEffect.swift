cdbeaf050adcb1c168c96fed787032492dafe2ee
Kelvin <kelvin.javorskisoares@gmail.com>
2020-08-21 15:03:14 -0300

    Auto stash before merge of "develop" and "origin/develop"

---
+++ Dragon Dreamers/Dragon Dreamers/Model/CardEffect.swift
@@ -0,0 +1,17 @@
+//
+//  CardEffect.swift
+//  Dragon Dreamers
+//
+//  Created by Kelvin Javorski Soares on 21/08/20.
+//  Copyright © 2020 Kelvin Javorski Soares. All rights reserved.
+//
+
+import Foundation
+
+class CardEffect{
+    var effectType : EffectType!
+    
+    
+    
+    
+}
