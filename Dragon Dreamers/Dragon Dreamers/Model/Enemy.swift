089f272bdd96618bbcb738b731c13690bfaa0219
Kelvin <kelvin.javorskisoares@gmail.com>
2020-08-23 20:39:40 -0300

    Add new cards effect and increment Enemy

---
+++ Dragon Dreamers/Dragon Dreamers/Model/Enemy.swift
@@ -0,0 +1,13 @@
+//
+//  Enemy.swift
+//  Dragon Dreamers
+//
+//  Created by Kelvin Javorski Soares on 23/08/20.
+//  Copyright © 2020 Kelvin Javorski Soares. All rights reserved.
+//
+
+import Foundation
+
+class Enemy: Person{
+    static let shared = Enemy()
+}
