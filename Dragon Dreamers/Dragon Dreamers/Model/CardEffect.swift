f8f69f468de925dedd742452c70138dc295c074e
Kelvin <kelvin.javorskisoares@gmail.com>
2020-08-28 16:15:35 -0300

    Fixes and cleaning code

--- Dragon Dreamers/Dragon Dreamers/Model/CardEffect.swift
+++ Dragon Dreamers/Dragon Dreamers/Model/CardEffect.swift
@@ -15,34 +15,36 @@ class CardEffect{
     
     func runEffects(card: Card, person: Person){
         switch card.effect {
-        case .addCard:
-            addCardToDeck(person: person, id: card.idEffect)
-            break
-            
-        case .dealDamage:
-            dealDamage(person: person, damage: card.damage, type: card.damageType)
-            break
-            
-        case .dealAndSufferDamage:
-            dealAndSufferDamage(person: person, damage: card.damage, damageType: card.damageType, selfDamage: card.selfDamage, selfDamageType: card.selfDamageType)
-            break
-        
-        case .heal:
-            heal(person: person, healAmount: card.heal, type: card.healType)
-            break
-            
-        default:
-            break
-            
+            //Add a specific card to deck
+            case .addCard:
+                addCardToDeck(person: person, id: card.idEffect)
+                break
+            //Deal damage to a person (or self)
+            case .dealDamage:
+                dealDamage(person: person, damage: card.damage, type: card.damageType)
+                break
+            //Deal and take damage
+            case .dealAndSufferDamage:
+                dealAndSufferDamage(person: person, damage: card.damage, damageType: card.damageType, selfDamage: card.selfDamage, selfDamageType: card.selfDamageType)
+                break
+            //Heal an atribute
+            case .heal:
+                heal(person: person, healAmount: card.heal, type: card.healType)
+                break
+            default:
+                break
         }
     }
     
-    func addCardToDeck(person:Person, id: Int){
+    func addCardToDeck(person:Person,
+                     id: Int){
         let cardsPool = person.cardsPool
         person.deck.addCard(cardsPool.cards[id])
     }
     
-    func heal(person: Person, healAmount : Int, type: EnergyType){
+    func heal(person: Person,
+             healAmount : Int,
+             type: EnergyType){
         switch type {
             case .life:
                 person.currentLife += healAmount
@@ -59,7 +61,9 @@ class CardEffect{
         }
     }
 
-    func dealDamage(person: Person, damage : Int, type: EnergyType){
+    func dealDamage(person: Person,
+                  damage : Int,
+                  type: EnergyType){
         switch type {
             case .life:
                person.currentLife -= damage
@@ -76,11 +80,14 @@ class CardEffect{
         }
     }
     
-    func dealAndSufferDamage(person: Person, damage: Int, damageType: EnergyType, selfDamage: Int, selfDamageType: EnergyType){
+    func dealAndSufferDamage(person: Person,
+                          damage: Int,
+                          damageType: EnergyType,
+                          selfDamage: Int,
+                          selfDamageType: EnergyType){
         
         dealDamage(person: person, damage: damage, type: damageType)
-        
-        dealDamage(person: person, damage: selfDamage, type: selfDamageType)
+        dealDamage(person: person.opponent, damage: selfDamage, type: selfDamageType)
     }
     
 }
