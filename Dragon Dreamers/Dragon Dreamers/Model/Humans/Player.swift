a38f474f2ac6ddf398ce9fdd3f0b9cd69505d8ea
Kelvin <kelvin.javorskisoares@gmail.com>
2020-09-15 14:41:14 -0300

    A wild version kind of working maybe se pá

--- Dragon Dreamers/Dragon Dreamers/Model/Humans/Player.swift
+++ Dragon Dreamers/Dragon Dreamers/Model/Humans/Player.swift
@@ -36,47 +36,4 @@ class Player: Person {
         currentUnderstanding = 0
         maxUnderstanding = 10
     }
-
-    
-//    func changeDeckOfCard (_ from: Deck, _ to: Deck, _ index: Int) {
-//        let card = from.getCard(index)
-//        from.removeCard(atIndex: index)
-//        to.addCard(card)
-//    }
-//
-//    func getNewDeckFromDiscart () { // Not in use
-//        discard.shuffle()
-//        while discard.cards.count > 0 {
-//            changeDeckOfCard(discard, deck, 0)
-//        }
-//    }
-//
-//    func getCardFromDiscard (_ index: Int) {
-//        changeDeckOfCard(discard, deck, index)
-//    }
-//
-//    func drawCards (amount : Int) {
-//        if deck.cards.count >= amount {
-//            for _ in 0 ..< amount {
-//                changeDeckOfCard(deck, hand, 0)
-//            }
-//        } else if deck.cards.count > 0 {
-//            for _ in 0 ..< deck.cards.count {
-//                changeDeckOfCard(deck, hand, 0)
-//            }
-//        }
-//    }
-//
-//    func discardHand () {
-//        while hand.cards.count > 0 {
-//            changeDeckOfCard(hand, discard, 0)
-//        }
-//    }
-//
-//    func playCard (index : Int) {
-//        let card = hand.cards[index]
-//        changeDeckOfCard(hand, ongoing, index)
-//        card.playCard()
-//    }
-    
 }
