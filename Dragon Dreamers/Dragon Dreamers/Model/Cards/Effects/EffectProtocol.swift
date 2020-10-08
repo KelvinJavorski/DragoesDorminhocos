64b3de05648d61b3b5ee7da66048d663a27da249
Kelvin <kelvin.javorskisoares@gmail.com>
2020-10-08 20:39:17 -0300

    Added a file to make a protocol, to use as a delegate

---
+++ Dragon Dreamers/Dragon Dreamers/Model/Cards/Effects/EffectProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  EffectProtocol.swift
+//  Dragon Dreamers
+//
+//  Created by Kelvin Javorski Soares on 08/10/20.
+//  Copyright © 2020 Kelvin Javorski Soares. All rights reserved.
+//
+
+import Foundation
+protocol EffectProtocol {
+    func applyEffects(card: Card)
+}
