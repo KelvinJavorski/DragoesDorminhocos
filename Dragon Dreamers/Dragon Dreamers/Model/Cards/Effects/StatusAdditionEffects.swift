1ec787712c62643415c6699ad1ec98d23130db2b
Kelvin <kelvin.javorskisoares@gmail.com>
2020-10-14 10:00:40 -0300

    Added new files

---
+++ Dragon Dreamers/Dragon Dreamers/Model/Cards/Effects/StatusAdditionEffects.swift
@@ -0,0 +1,9 @@
+//
+//  StatusAdditionEffects.swift
+//  Dragon Dreamers
+//
+//  Created by Kelvin Javorski Soares on 09/10/20.
+//  Copyright © 2020 Kelvin Javorski Soares. All rights reserved.
+//
+
+import Foundation
---
+++ Dragon Dreamers/Dragon Dreamers/Model/Enums/BattleTurnStatus.swift
@@ -0,0 +1,9 @@
+//
+//  BattleTurnStatus.swift
+//  Dragon Dreamers
+//
+//  Created by Kelvin Javorski Soares on 13/10/20.
+//  Copyright © 2020 Kelvin Javorski Soares. All rights reserved.
+//
+
+import Foundation
