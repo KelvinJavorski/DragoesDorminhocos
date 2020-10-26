abe0bea61608a3da5dc6b98c5830505058a71e21
Kelvin <kelvin.javorskisoares@gmail.com>
2020-10-26 15:11:07 -0300

    Fix for kevinho

--- Dragon Dreamers/Dragon Dreamers/Model/Cards/Effects/AddStatusEffect.swift
+++ Dragon Dreamers/Dragon Dreamers/Model/Cards/Effects/AddStatusEffect.swift
@@ -8,30 +8,6 @@
 
 import Foundation
 
-class AddPlayBlueCardTwiceStatus: EffectProtocol{
-    func applyEffects(card: Card) {
-        card.owner.currentStatus.append(.blue)
-    }
-}
-
-class AddPlayGreenCardTwiceStatus: EffectProtocol{
-    func applyEffects(card: Card) {
-        card.owner.currentStatus.append(.green)
-    }
-}
-
-class AddPlayRedCardTwiceStatus: EffectProtocol{
-    func applyEffects(card: Card) {
-        card.owner.currentStatus.append(.red)
-    }
-}
-
-class AddPlayYellowCardTwiceStatus: EffectProtocol{
-    func applyEffects(card: Card) {
-        card.owner.currentStatus.append(.yellow)
-    }
-}
-
 class AddReasoningStatus: EffectProtocol{
     let value: Int
     init(value: Int) {
