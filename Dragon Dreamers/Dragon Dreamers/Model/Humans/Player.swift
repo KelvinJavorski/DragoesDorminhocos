12d76f337d2734e0c31c86adb89c7aa4eed8a23c
Kelvin <kelvin.javorskisoares@gmail.com>
2020-10-27 00:33:15 -0300

    Added instance of every Way. Added func to get a random Way

--- Dragon Dreamers/Dragon Dreamers/Model/Humans/Player.swift
+++ Dragon Dreamers/Dragon Dreamers/Model/Humans/Player.swift
@@ -11,7 +11,10 @@ import Foundation
 class Player: Person {
     static let shared = Player()
     var manaManager: ManaManager = ManaManager()
-    
+    var sol = Way(name: "Sol")
+    var areia = Way(name: "Areia")
+    var brisa = Way(name: "Brisa")
+    var oceano = Way(name: "Oceano")
     init () {
         super.init()
         print(">>> Init Player...")
@@ -44,8 +47,23 @@ class Player: Person {
         let imposition = Token(name: "Imposição", amount: 0, way: .sol)
         let distant = Token(name: "Distante", amount: 0, way: .areia)
         let doubt = Token(name: "Dúvida", amount: 0, way: .brisa)
-        let conection = Token(name: "Conexão", amount: 0, way: .oceano)
-        tokens = [imposition, distant, doubt, conection]
+        let connection = Token(name: "Conexão", amount: 0, way: .oceano)
+        tokens = [imposition, distant, doubt, connection]
+    }
+    
+    func getRandomWay(except ways: [String]) -> Way{
+        var allWays = [sol, areia, brisa, oceano]
+        allWays.filter {
+            for (i , way) in ways.enumerated(){
+                if way == $0.name{
+                    allWays.remove(at: i)
+                    return true
+                }
+            }
+            return false
+        }
         
+        let randomNumber = Int.random(in: 0..<allWays.count)
+        return allWays[randomNumber]
     }
 }
