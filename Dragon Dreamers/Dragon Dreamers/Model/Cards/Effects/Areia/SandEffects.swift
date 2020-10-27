0d0642b9efbd7a52f50fe0dcb056da4622244746
Kelvin <kelvin.javorskisoares@gmail.com>
2020-10-27 00:34:08 -0300

    Add Sand Effects

--- Dragon Dreamers/Dragon Dreamers/Model/Cards/Effects/Areia/SandEffects.swift
+++ Dragon Dreamers/Dragon Dreamers/Model/Cards/Effects/Areia/SandEffects.swift
@@ -8,15 +8,63 @@
 
 import Foundation
 
-class Desviar : EffectProtocol{
+class DesviarStatus : EffectProtocol{
     func applyEffects(card: Card) {
-        let opponent = card.owner.opponent
-        opponent?.currentStatus.append(.distant)
+        let opponent = card.owner.opponent!
+        let status = Status(name: "Distante", duration: 1, target: opponent, effect: DesviarEffect())
+        opponent.status.append(status)
+    }
+}
+
+class DesviarEffect : EffectProtocol{
+    func applyEffects(card: Card) {
+        print("cancela efeito da proxima carta da vo")
     }
 }
 
 class Defender : EffectProtocol{
     func applyEffects(card: Card) {
-        <#code#>
+        let person = Player.shared
+        let randomWay = person.getRandomWay(except: ["Areia"])
+        randomWay.changeBlockStatus(true)
+    }
+}
+
+class Fugir : EffectProtocol{
+    func applyEffects(card: Card) {
+        let person = card.owner!
+        let count = person.hand.cards.count
+        person.discardHand()
+        Player.shared.areia.increaseAmount(amount: count)
+        
+    }
+}
+
+class PrevenirStatus : EffectProtocol{
+    func applyEffects(card: Card) {
+        let person = card.owner!
+        let status = Status(name: "Prevenir", duration: 1, target: person, effect: PrevenirEffect())
+        person.status.append(status)
+    }
+}
+
+class PrevenirEffect : EffectProtocol {
+    func applyEffects(card: Card) {
+        let person = Player.shared
+        Player.shared.discardHand()
+        person.hand.addCards(person.lastHand.cards)
+    }
+}
+
+class Evitar : EffectProtocol{
+    func applyEffects(card: Card) {
+        let person = Player.shared
+        person.areia.increaseAmount(amount: 1)
+        for status in person.status{
+            if status.name == "Distante"{
+                status.increaseDuration(amount: 1)
+                break
+            }
+        }
     }
 }
